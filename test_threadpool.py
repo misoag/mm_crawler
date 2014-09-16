5893150a8a481ea9caf367338ba71aed42943ceb
misoag <misoag@126.com>
2014-09-16 18:54:42 +0800

    - test

--- test_threadpool.py
+++ test_threadpool.py
@@ -7,13 +7,21 @@ import sys
 sys.path.append("threadpool/src")
 from threadpool import *
 
+
+main = ThreadPool(10)
+
 def doWork(i):
     time.sleep(i)
     print threading.currentThread().getName()," has sleeped ",i," seconds"
     print "Thread count:",len(threading.enumerate()),"\r\n"
+    if(i == 4):
+        main.dismissWorkers(10)
 
-pool = ThreadPool(10)
-for i in range(60):
-    pool.putRequest(WorkRequest(doWork, [i]))
 
-pool.wait()
+for i in range(10):
+    main.putRequest(WorkRequest(doWork, [i]))
+
+try:
+    main.wait()
+except NoWorkersAvailable:
+    None
