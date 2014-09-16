9131c2545ff56acc9b5371960f6953fc45553c54
misoag <misoag@126.com>
2014-09-17 00:17:28 +0800

    - mm_crawler

--- mm_crawler.py
+++ mm_crawler.py
@@ -3,127 +3,71 @@
 
 import sys
 import os
-import getopt
 import re
 import urllib
 import urllib2
 import urlparse
-import ThreadPool
 import subprocess
+import threading
+import helper
+
+sys.path.append("threadpool/src")
+from threadpool import *
 
 class MMCrawler:
 
-    M_THREAD_SIZE      = 10
-    M_OUTPUT_DIRECTORY = "pics"
-    M_IMAGES_LIMIT     = 0
-    M_TASKS_COUNT      = 0
-    M_TASKS_STOP       = False
-        
-    def help(self):
-        print "用法: python /path/mm_crawler.py [参数]"
-        print "参数:"
-        print "     -h                    帮助"
-        print "     -n [size]             线程并发数"
-        print "     -o [path]             图片保存目录"
-        print "     -l [size]             抓取图片的个数"
-
-    def options(self):
-        optlist, args = getopt.getopt(sys.argv[1:], 'hn:o:l:')
-        for op,value in optlist:
-            if op == "-h":
-                self.help()
-                sys.exit()
-            elif op == "-n":
-                    MMCrawler.M_THREAD_SIZE         = int(value)
-            elif op == "-o":
-                    MMCrawler.M_OUTPUT_DIRECTORY    = value
-            elif op == "-l":
-                    MMCrawler.M_IMAGES_LIMIT        = int(value)
-
-    def http_get(self,url):
-        try:
-            req     = urllib2.Request(url)
-            opener  = urllib2.build_opener()
-            f       = opener.open(req,timeout=10)
-            isGzip  = f.headers.get('Content-Encoding')
-            if isGzip :
-                compresseddata      = f.read()
-                compressedstream    = StringIO.StringIO(compresseddata)
-                gzipper             = gzip.GzipFile(fileobj=compressedstream)
-                data                = gzipper.read()
-            else:
-                data = f.read()
-
-            return data
-        except:
-            print "[Error]: [request ",url,"]",sys.exc_info()[:2]
-            return ""
-
-
-    def main(self):
-        self.options()
-        #初始化线程池
-        self.wm = ThreadPool.WorkerManager(MMCrawler.M_THREAD_SIZE,timeout=10)
-        #按分类抓取
-        self.recursion_category("http://www.22mm.cc/mm/qingliang/index.html");
-        self.recursion_category("http://www.22mm.cc/mm/jingyan/index.html");
-        self.recursion_category("http://www.22mm.cc/mm/bagua/index.html");
-        self.recursion_category("http://www.22mm.cc/mm/suren/index.html");
+    def __init__(self,threads,output,limits):
+        self.threads = threads #任务线程数
+        self.output = output #图片保存目录
+        self.limits = limits  #抓取图片数量限制
+        self.tasks = 0 #已完成任务数
+        self.stop = False #停止任务
+        self.threadpool = ThreadPool(self.threads) #初始化线程池
 
     def recursion_category(self,category_index_page):
-        ''' 递归抓取所有分类'''
+        """递归抓取所有分类页面
+        :param category_index_page: 分类首页地址
+        """
         while(True):
-            if(MMCrawler.M_TASKS_STOP):
-                break;
-            #当前页
-            self.fetch_category_page(category_index_page)
-            #下一页
-            nextpage = self.get_next_category_page(category_index_page)
-            if nextpage != '':
+            if self.stop == True: break;
+            self.fetch_category_page(category_index_page) #抓取当前页
+            nextpage = self.get_next_category_page(category_index_page) #获得下一页地址
+            if nextpage != "":
                 self.recursion_category(nextpage)
             else:
-                break;
+                break
 
     def fetch_category_page(self,category_page_url):
-        '''
-            1、抓取当前页面中的所有套图首页
-            2、抓取套图中的各图片地址
-            3、将抓取任务丢入线程池
-        '''
+        """抓取分类页中的所有图片
+        :param category_page_url: 分页类地址
+        """
         suites  = self.fetch_suites(category_page_url) 
-        print "套图数量：",len(suites)
+        print "当前分类页中套图数量：",len(suites)
         for suite_url in suites:
             suite_images = self.fetch_suite_images(suite_url) 
-            print "图片数量：",len(suite_images)
+            print "当前套图中图片数量：",len(suite_images)
             for image_url in suite_images:
-                #将任务加入到线程池
-                self.wm.add_job(self.save_image,MMCrawler.M_OUTPUT_DIRECTORY,image_url)
-                #超过限制数量则退出
-                MMCrawler.M_TASKS_COUNT += 1
-                if MMCrawler.M_IMAGES_LIMIT != 0 and MMCrawler.M_TASKS_COUNT >= MMCrawler.M_IMAGES_LIMIT:
-                    MMCrawler.M_TASKS_STOP = True
-                    self.wm.wait_for_complete()
+                if not self.stop:
+                    self.fetch_image(image_url)
+                else:
                     return
-    
 
     def get_next_category_page(self,category_page_url):
-        '''
-            给出一个大类地址,返回下一页的地址。
-            如果是最后一页，则返回空字符串。
-        '''
-        html            = self.http_get(category_page_url)
+        """获取分类的下一页地址
+        :param category_page_url: 分类页地址
+        """
+        html            = helper.http_get(category_page_url)
         next_page_re    = re.compile('<a href=\'(index_\\d+.html)\'>></a>')
         next_page       = next_page_re.findall(html)
-        if(len(next_page)>0): 
-            return urlparse.urljoin(category_page_url,next_page[0]);
+        if len(next_page) > 0:
+            return urlparse.urljoin(category_page_url,next_page[0])
         return ""
 
-
     def fetch_suites(self,category_page_url):
-        '''
-            给出分类的页面,返回所有套图首页
-        '''
-        html = self.http_get(category_page_url)
+        """抓取分类页中的所有套图
+        :param category_page_url: 分类页地址
+        """
+        html = helper.http_get(category_page_url)
         real_urls = []
         link_re   = re.compile('<a.?href="(.*?)"')
         link_list = link_re.findall(html)
@@ -134,12 +78,11 @@ class MMCrawler:
                 real_urls.append(realurl)
         return real_urls
 
-
     def fetch_suite_images(self,pageurl):
-        '''
-            给出套图首页,返回所有图片列表
-        '''
-        html = self.http_get(pageurl)
+        """获取套图中的所有图片列表
+        :param pageurl: 套图首页地址
+        """
+        html = helper.http_get(pageurl)
         if html == "":  return []
         #获得图片数量
         image_count_re  = re.compile('</span>/(\\d+)</strong><a href=')
@@ -147,7 +90,7 @@ class MMCrawler:
         image_count     = len(image_count) == 1 and image_count[0] or 0
         #分析:最后一页中包含了所有的图片地址,因此抓取最后一页即可
         pageurl = pageurl[0:-5] + '-' + image_count + '.html'
-        html    = self.http_get(pageurl)
+        html    = helper.http_get(pageurl)
         if html == "":  return []
         #解析出所有图片地址
         link_re   = re.compile('\]="(.*?)"')
@@ -155,21 +98,52 @@ class MMCrawler:
         link_list = map(lambda url: url.replace("big","pic"),link_list)
         return link_list
 
+    def fetch_image(self,image_url):
+        """抓取网络图片
+        :param image_url: 图片地址
+        """
+        if not self.__completed():
+            #创建图片保存目录
+            output_dir,filename = helper.make_subdir(self.output,image_url)
+            realpath = os.path.join(output_dir,filename)
+            #将抓取任务放入线程池
+            self.threadpool.putRequest(WorkRequest(self.__retrieve_net_image,[realpath,image_url]))
+        else:
+            self.threadpool.wait()
 
-    def save_image(self,save_dir,image_url):
-        #根目录
-        output_dir = os.path.join(os.getcwd(),save_dir)
-        #子目录
-        split_result    = urlparse.urlsplit(image_url)
-        relative_path   = os.path.split(split_result.path)[0].replace("/pic/","")
-        image_dir       = os.path.join(output_dir,relative_path)
-        #保存图片
-        command = "mkdir -p "+image_dir+" && cd "+image_dir + " && curl -O " + image_url
-        subprocess.check_output(command,shell=True)
-        #方式2
-        #urllib.urlretrieve(image_url,filepath)
-        return image_url
+    def __retrieve_net_image(self,realpath,image_url):
+        """将网络图片并保存到本地目录(该函数在将会多线程访问)
+        :param realpath: 保存到本地的绝对路径
+        :param image_url: 图片网络地址
+        """
+        thread_current = threading.current_thread().getName()
+        # print "current_thread: ",thread_current
+        try:
+            urllib.urlretrieve(image_url,realpath)
+        except:
+           print "[Error]:[retrieve_net_image]",sys.exc_info()[:2]
 
+    def __completed(self):
+        """检查是否满足条件停止程序"""
+        if  self.stop == False and self.limits != 0 and self.tasks >= self.limits:
+            self.stop = True
+            return True
+        else:
+            print "Limits:",self.limits,"Threads:",len(threading.enumerate())," Completed:",self.tasks
+            self.tasks += 1
+            return False
 
 if __name__ == "__main__":
-    MMCrawler().main()
+    options = helper.get_options()
+    mm_crawler = MMCrawler(options.threads,options.output,options.limits)
+    #22MM美女网
+    # mm_crawler.recursion_category("http://www.22mm.cc/mm/qingliang/index.html")
+    # mm_crawler.recursion_category("http://www.22mm.cc/mm/jingyan/index.html")
+    # mm_crawler.recursion_category("http://www.22mm.cc/mm/bagua/index.html")
+    # mm_crawler.recursion_category("http://www.22mm.cc/mm/suren/index.html")
+
+    #美女私房照
+    mm_crawler.recursion_category("http://www.mnsfz.com/h/qingchun/index.html")
+    # mm_crawler.recursion_category("http://www.mnsfz.com/h/meihuo/index.html")
+    # mm_crawler.recursion_category("http://www.mnsfz.com/h/yangguang/index.html")
+    # mm_crawler.recursion_category("http://www.mnsfz.com/h/qiaopi/index.html")
